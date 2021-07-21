d08cb8078b6d817b76d6c075016b65b03f2bd880
Vladislav <vlad.starikov.88@gmail.com>
2021-07-21 23:39:39 +0300

    add current location

--- src/components/core/AppBar.vue
+++ src/components/core/AppBar.vue
@@ -4,18 +4,29 @@
   >
     <div class="menu">
       <div class="menu-element" v-for="link in menuLinks" :key="link.value">
-        <router-link :to="link.value">{{link.text}}</router-link>
+        <router-link
+          :to="link.value"
+          :class="{
+            'current-path': currentLocation === link.value
+          }"
+        >{{link.text}}</router-link>
       </div>
     </div>
   </v-app-bar>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 
 export default defineComponent({
   name: 'App',
   setup() {
+    const router = useRouter();
+    const route = useRoute();
+    // TODO don't works
+    const currentLocation = computed(() => router.options.history.location || route);
+
     const menuLinks = [
       {
         text: 'Home',
@@ -27,6 +38,7 @@ export default defineComponent({
       },
     ];
     return {
+      currentLocation,
       menuLinks,
     };
   },
@@ -48,6 +60,12 @@ export default defineComponent({
       &:hover {
         color: rgb(30, 30, 30);
         background-color: rgb(230, 230, 230);
+      }
+      &.current-path {
+        color: #000;
+        &:hover {
+          color: #000;
+        }
       }
     }
   }
